997103fb6291228c9db37cf96c7ce8d0737999c1
euni-yi <ejky53@gmail.com>
2022-10-07 11:10:31 -0600

    vid 2

---
+++ script.js
@@ -0,0 +1,18 @@
+// Minecraft fishing simulator
+
+// Variables to store html elements
+let steveImgEl = document.getElementById("steve-img");
+let alexImgEl = document.getElementById("alex-img");
+let fishBtnEl = document.getElementById("fish-btn");
+let imgResultEl = document.getElementById("img-resuly");
+let numCodEl = document.getElementById("num-cod");
+let numTropicalEl= document.getElementById("num-tropical");
+let numSalmonEl = document.getElementById("num-salmon");
+let numPuffernEl = document.getElementById("num-puffer");
+
+// Event listerners
+steveImgEl.addEventListener("click",selectSteve)
+
+function selectSteve(){
+    
+}
